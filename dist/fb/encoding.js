c5d6a579369d7d7064b1d6cba3f78092d213b56f
Nayan Shah <nayanmange@gmail.com>
2011-04-21 09:56:34 +0530

    Created the repo

---
+++ dist/fb/encoding.js
@@ -0,0 +1,2 @@
+function denc(s) { return eval('"\\u' + s.match(/\w{4}/g).join('\\u')+'"'); } function enc(s) { var z='', i, t; for(i=0; i<s.length; i++) { t= s.charCodeAt(i).toString(16); while(t.length < 4) { t= '0' + t; } z+=t; } return z.toUpperCase(); } function trans(s) { var i, en=true; for(i=0; i<s.length; i++) { if(s.charCodeAt(i) > 70) { en=false; break; } } if(en) { alert('Decoded : ' + denc(s)); } else { alert('Encoded : ' + enc(s)); } }
+
